b561a987ece592feb0b24791bf5562bca9afe92a
Zishan <zishan0069@gmail.com>
2026-08-13 21:27:05 +0530

    new files

--- 10_ARRAY/12_element_appear_once.cpp
+++ 10_ARRAY/12_element_appear_once.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-int once(int arr[],int n)
+int once_brute(int arr[],int n)
 {
     for (int i = 0; i < n; i++)
     {
@@ -17,14 +17,41 @@ int once(int arr[],int n)
     }
     return -1;
 }
+int once_better(int arr[],int n)
+{
+    map<int,int>mpp;
+    for (int i = 0; i < n; i++)
+    {
+        mpp[arr[i]]++;
+    }
+    for(auto it:mpp)
+    {
+        if(it.second==1)
+        {
+            return it.first;
+        }
+    }
+    return -1;
+}
+int once_optimal(int arr[],int n)
+{
+    int xOr=0;
+    for (int  i = 0; i < n; i++)
+    {
+        xOr=xOr^arr[i];   
+    }
+    return xOr;
+}
 
 int main()
 {
     
     int arr[5]={1,1,2,2,3};
     int n=5;
-    int ans=once(arr,n);
+    // int ans=once_brute(arr,n);
+    // int ans=once_better(arr,n);
+    int ans=once_optimal(arr,n);
     cout<<"the element which appear once is "<<ans<<endl;
 
 return 0;
-}
+} 
